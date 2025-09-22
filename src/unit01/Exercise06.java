4a69a0591534900c4a9a9cc14d4a282e4b216524
nicolas.mingorance <nicolas.mingorance@106-16.info2>
2025-09-22 12:32:24 +0200

    Actualización. Ejercicio 6

---
+++ src/unit01/Exercise06.java
@@ -0,0 +1,48 @@
+//Escribir un programa que le pida dos números al usuario. A continuación, debe mostrar la suma, la resta, la multiplicación y la división de ambos números. Debe mostrarse el resultado de cada operación en una línea distinta.
+
+package unit01;
+
+import java.util.Scanner;
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		
+		//Create a new scanner
+		Scanner sc = new Scanner(System.in);
+				
+		//We declare the variables n1 and n2
+		double n1, n2;
+		double sum, subtraction, multiplication, division;
+				
+		//We ask for n1 
+		System.out.print("Enter the value 1: ");
+		n1 = sc.nextDouble();
+		
+		//We ask for n2
+		System.out.print("Enter the value 2: ");
+		n2 = sc.nextDouble();
+		
+		//We calculate the sum
+		sum = n1 + n2;
+		
+		//We calculate the subtraction
+		subtraction = n1 - n2;
+		
+		//We calculate the multiplication
+		multiplication = n1 * n2;
+		
+		//We calculate the division
+		division = n1 / n2;
+		
+		//We display on the screen the values
+		System.out.println("Sum = " + sum);
+		System.out.println("Subtraction = " + subtraction);
+		System.out.println("Multiplication = " + multiplication);
+		System.out.println("The division = " + division);
+		
+		// Close the scanner
+        sc.close();	
+	}
+
+}
