83e21ceb92065829de0fa8a17b6d4b0f2a99eead
nicolas.mingorance <nicolas.mingorance@106-16.info2>
2025-09-22 12:16:24 +0200

    Actualización. Ejercicio 5.

---
+++ src/unit01/Exercise05.java
@@ -0,0 +1,35 @@
+//Diseñar una aplicación que calcule la longitud y el área de una circunferencia. Para ello, el usuario debe introducir el radio, que puede contener decimales. Usa Math.PI para tomar el valor de PI. (longitud = 2πr, área=πr2)
+
+package unit01;
+
+import java.util.Scanner;
+
+public class Exercise05 {
+
+	public static void main(String[] args) {
+		
+		//Create a new scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//We declare the variables radius, length and area
+		double radius;
+		double length;
+		double area;
+		
+		//We ask for radius
+		System.out.print("Enter the radius of the circumference: ");
+		radius = sc.nextDouble();
+		
+		//We calculate the length and the area
+		length = 2 * Math.PI * radius;
+		area = Math.PI * radius * radius;
+		
+		//We display on the screen the value of the length and the area.
+		System.out.println("The length is: " + length + " units");
+		System.out.print("The area is: " + area + " square units");
+		
+		// Close the scanner
+        sc.close();		
+	}
+
+}
